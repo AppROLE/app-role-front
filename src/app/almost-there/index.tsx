6b713376d8b736d8072f2af77843b3b9f289decd
João <galhardojoaopedro@gmail.com>
2024-08-27 12:18:08 -0300

    feat: Add AlmostThere component

---
+++ src/app/almost-there/index.tsx
@@ -0,0 +1,15 @@
+import Background from "@/src/components/background";
+import { Text, View } from "react-native";
+
+
+
+
+export default function AlmostThere() {
+    return (
+        <>
+            <Background>
+                
+            </Background>
+        </>
+    )
+}
