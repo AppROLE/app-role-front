9ff3502da08915be49ad8690b6992039936add71
João <galhardojoaopedro@gmail.com>
2024-08-27 12:57:28 -0300

    feat: Add Background component to CreateNewPassword page

--- src/app/create-new-password/index.tsx
+++ src/app/create-new-password/index.tsx
@@ -1,9 +1,10 @@
-
+import Background from "@/src/components/background";
 
 export default function CreateNewPassword() {
     return (
         <>
-        
+        <Background>
+        </Background>
         </>
     )
 }
