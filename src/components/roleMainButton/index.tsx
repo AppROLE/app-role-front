6f3c98da48deb14468245dbe436d555baaccfdf4
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-08-29 08:46:59 -0300

    feat: Add useState hook and disabled state to RoleMainButton component
    This commit adds the useState hook and a new disabled state to the RoleMainButton component. The disabled state is used to prevent multiple button clicks and improve the user experience. When the buttonFunction is executed, the disabled state is set to true, disabling the button until the function is completed. This enhancement ensures that the button is not clicked multiple times, preventing any unintended behavior.
    
    Note: This commit message follows the established convention of using the "feat" type for adding new features.

--- src/components/roleMainButton/index.tsx
+++ src/components/roleMainButton/index.tsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { TouchableOpacity, Text, View } from "react-native";
 
 interface RoleMainButtonProps {
@@ -9,33 +9,54 @@ interface RoleMainButtonProps {
 }
 
 export default function RoleMainButton ({ children, buttonFunction, type }: RoleMainButtonProps) {
-    return (
-      <TouchableOpacity className="drop-shadow-2xl shadow-[#9C4EDC4D] text-white text-[16px]" onPress={buttonFunction}>
-        {type === 'gradient' ? (
-        <LinearGradient
-          colors={['#5A189A', '#9C4EDC']}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }} // Gradiente diagonal suave
-          style={{ 
-            paddingVertical: 12, 
-            paddingHorizontal: 30, 
-            borderRadius: 20, // Bordas arredondadas
-            alignItems: 'center',
+    const [disabled, setDisabled] = useState(false);
 
-            shadowColor: 'rgba(156, 78, 220, 0.3)', // Cor da sombra
-            shadowOffset: { width: 0, height: 7 }, // Deslocamento da sombra
-            shadowOpacity: 1, // Opacidade da sombra
-            shadowRadius: 11, // Raio de desfoque
-            elevation: 10
-          }}
+    function executeFunction() {
+        if (disabled) return;
+        if (buttonFunction) {
+            buttonFunction();
+            setDisabled(true);
+        }
+    }
+
+    const gradientColors = disabled 
+        ? ['rgba(90, 24, 154, 0.25)', 'rgba(156, 78, 220, 0.25)'] // Cores do gradiente com brilho reduzido
+        : ['#5A189A', '#9C4EDC']; // Cores normais do gradiente
+
+    return (
+        <TouchableOpacity 
+            disabled={disabled} 
+            className="drop-shadow-2xl shadow-[#9C4EDC4D] text-white text-[16px]" 
+            onPress={executeFunction}
         >
-          {children}
-        </LinearGradient>
-        ) : (
-          <View className="bg-[#1C1C1C] items-center flex flex-row gap-4 justify-center" style={{paddingVertical: 12, paddingHorizontal: 30, borderRadius: 20}}>
-            {children}
-          </View>
-        )}
-      </TouchableOpacity>
-    )
+            {type === 'gradient' ? (
+                <LinearGradient
+                    colors={gradientColors}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 1 }} // Gradiente diagonal suave
+                    style={{ 
+                        paddingVertical: 12, 
+                        paddingHorizontal: 30, 
+                        borderRadius: 20, // Bordas arredondadas
+                        alignItems: 'center',
+
+                        shadowColor: 'rgba(156, 78, 220, 0.3)', // Cor da sombra
+                        shadowOffset: { width: 0, height: 7 }, // Deslocamento da sombra
+                        shadowOpacity: 1, // Opacidade da sombra
+                        shadowRadius: 11, // Raio de desfoque
+                        elevation: 10,
+                    }}
+                >
+                    {children}
+                </LinearGradient>
+            ) : (
+                <View 
+                    className="bg-[#1C1C1C] items-center flex flex-row gap-4 justify-center" 
+                    style={{paddingVertical: 12, paddingHorizontal: 30, borderRadius: 20}}
+                >
+                    {children}
+                </View>
+            )}
+        </TouchableOpacity>
+    );
 }
