840b05e2f5c2a75678439ba8f151050814d65a54
Breno1101 <brenocoutinhorodrigues@gmail.com>
2024-08-29 19:04:03 -0300

    adjusting button 'enviar' and toast for the message errors

--- src/app/forgot-password/index.tsx
+++ src/app/forgot-password/index.tsx
@@ -1,12 +1,14 @@
 import Background from "@/src/components/background";
-import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
-import Entypo from '@expo/vector-icons/Entypo';
+import { Text, TouchableOpacity, View, Keyboard } from "react-native";
 import { useState } from "react";
 import { Link } from "expo-router";
-
+import RoleInput from "@/src/components/input";
+import Toast from 'react-native-toast-message';
+import GradientButton from "@/src/components/gradientButton";
 
 export default function ForgotPassword() {
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
 
     const validateEmail = (email: string) => {
         const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -15,46 +17,44 @@ export default function ForgotPassword() {
 
     const handlePress = () => {
         if (validateEmail(email)) {
-            Alert.alert(
-                "Sucesso",
-                "E-mail enviado com sucesso. Verifique sua caixa de entrada.",
-                [{ text: "OK" }]
-            );
+                Keyboard.dismiss();
+                Toast.show({    
+                    type: 'success',
+                    text1: 'Sucesso',
+                    text2: 'E-mail enviado com sucesso!',
+                    visibilityTime: 3000,
+                    topOffset: 0,
+                });
             setEmail("");
         } else {
-            Alert.alert(
-                "Erro",
-                "Por favor, insira um e-mail válido.",
-                [{ text: "OK" }]
-            );
+            setEmailError("Por favor, insira um e-mail válido.");
         }
     };
+    function handleEmailChange(text: string) {
+        setEmail(text);
+        if (emailError) setEmailError(''); 
+    }
 
     return (
         <>
             <Background>
+                <Toast/>
                 <View className="flex justify-center items-center w-[80%]">
                     <Text className="text-white text-2xl ">Recuperação de Senha</Text>
                     <View className="w-[85%] pt-8">
                         <Text className="text-[#BDBDBD] text-center text-base">Escreva seu e-mail ou nome de usuário e enviaremos detalhes de como prosseguir 
                         para a criação de sua nova senha.</Text>
                     </View>
-                    <View className="flex w-80 flex-row gap-2 border-b-[1px] border-WHITE mt-16">
-                        <Entypo name="mail" size={24} color="#BDBDBD" />
-                        <TextInput
-                            placeholder="E-mail"
-                            className="w-[80%] text-white placeholder:text-[#BDBDBD]"
+                    <View className="flex w-80 flex-row gap-2 mt-16">
+                        <RoleInput
+                            type="email"
                             value={email}
-                            onChangeText={setEmail}
+                            onChangeText={handleEmailChange}
+                            error={emailError}
                         />
                     </View>
                     <View className="pt-16">
-                        <TouchableOpacity 
-                            className="mt-8 bg-violet-700 w-96 py-2 rounded-2xl h-10"
-                            onPress={handlePress}
-                        >
-                            <Text className="text-white text-center text-base">Enviar</Text>
-                        </TouchableOpacity>
+                        <GradientButton texto="Enviar" buttonFunction={handlePress}/>
                         <Link href={'/sign-in'} className="mt-8" asChild>
                             <TouchableOpacity className=" bg-[#1C1C1C] w-96 rounded-2xl h-10 justify-center items-center">
                                 <Text className="text-[#BDBDBD] text-center text-base">Voltar</Text>
