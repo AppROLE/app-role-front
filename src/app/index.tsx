1bc11078dcb4f5ff926f1e572a0eca2b85b0a7f2
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-08-29 08:46:42 -0300

    feat: Add lazy loading for images in Index component
    This commit adds lazy loading functionality for images in the Index component. By implementing lazy loading, the images will only be loaded when they are visible on the screen, improving the page load performance.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,14 @@
 import { Text, View } from 'react-native'
 import { Link } from 'expo-router'
+import RoleCard from '../components/roleCard'
+import CategoryCard from '../components/category-musicalCard'
+import RoleMainButton from '../components/roleMainButton'
 
 export default function Index() {
+  function teste () {
+    console.log('teste')
+  }
+
   return (
     <View className="flex h-full w-full items-center justify-center bg-blue-100">
       <Text className="text-2xl font-bold text-red-500">
@@ -10,6 +17,14 @@ export default function Index() {
       <Link href={'/sign-in'}>Sign In</Link>
       <Link href={'/almost-there'}>Almost there</Link>
       <Link href={'/confirm-forgot-password'}>Confirm Forgot Password</Link>
+      {/* View para testar os components */}
+      <View className='my-5 bg-[#121212] w-full'>
+        {/* <RoleMainButton type='gradient' buttonFunction={teste}>
+          <Text className='text-white text-base'>AAA</Text>
+        </RoleMainButton> */}
+        {/* <RoleCard data={'16 DEZ'} image={'https://placehold.co/600x400'} title={'São Conrado'} type={'Bar'} stars={4.5} local={'Itaim'} idRole={'1'}/> */}
+        {/* <CategoryCard type='Musical' name='Pagode'/> */}
+      </View>
     </View>
   )
 }
