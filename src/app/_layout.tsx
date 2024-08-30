02777b8ed02529179a6583b8c11565f2b683ee3f
Gabriel Merola <gabrielmerola12@gmail.com>
2024-08-29 22:34:46 -0300

    resolve routes opening

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,11 +5,27 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="sign-in/index" options={{ headerShown: false }} />
-      <Stack.Screen name="almost-there/index" options={{ headerShown: false }}/>
-      <Stack.Screen name="create-new-password/index" options={{ headerShown: false }}/>
-      <Stack.Screen name="forgot-password/index" options={{ headerShown: false }}/>
-      <Stack.Screen name="recovery-code/index" options={{ headerShown: false }}/>
-      <Stack.Screen name="confirm-forgot-password/index" options={{ headerShown: false }}/>
+      <Stack.Screen name="opening/index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="almost-there/index"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="create-new-password/index"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="forgot-password/index"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="recovery-code/index"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="confirm-forgot-password/index"
+        options={{ headerShown: false }}
+      />
     </Stack>
   )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -14,13 +14,14 @@ export default function Index() {
       <Text className="text-2xl font-bold text-red-500">
         EH FRONTAS DO APP ROLE
       </Text>
+      <Link href={'/opening'}>Opening</Link>
       <Link href={'/sign-in'}>Sign In</Link>
       <Link href={'/almost-there'}>Almost there</Link>
       <Link href={'/confirm-forgot-password'}>Confirm Forgot Password</Link>
       <Link href={'/sign-up'}>Sign Up</Link>
       <Link href={'/recovery-code'}>Recovery Code</Link>
       {/* View para testar os components */}
-      <View className='my-5 bg-[#121212] w-full'>
+      <View className="my-5 w-full bg-[#121212]">
         {/* <RoleMainButton type='gradient' buttonFunction={teste}>
           <Text className='text-white text-base'>AAA</Text>
         </RoleMainButton> */}
