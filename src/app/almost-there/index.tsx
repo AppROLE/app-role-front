1b90e1271fceef9419d5a0f1083a5ba55f43cf6d
João <galhardojoaopedro@gmail.com>
2024-09-03 11:11:26 -0300

    feat: Add SafeAreaView to 'AlmostThere' screen for better layout handling

--- src/app/almost-there/index.tsx
+++ src/app/almost-there/index.tsx
@@ -9,7 +9,8 @@ import {
     TextInput,
     TouchableOpacity,
     View,
-    StyleSheet
+    StyleSheet,
+    SafeAreaView
 } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import RoleMainButton from '@/src/components/roleMainButton'
@@ -51,83 +52,85 @@ export default function AlmostThere() {
     return (
         <>
             <Background>
-                <View>
-                    <Text className="text-3xl text-white">Estamos quase lá...</Text>
-                </View>
-                <View className="mb-5 mt-10 rounded-full bg-button_color">
-                    <Text className="p-2 text-xl text-white">
-                        Adicione uma foto de perfil!
-                    </Text>
-                </View>
-                <View className='relative flex items-center'>
-                    <LinearGradient
-                        style={{ borderRadius: 999, padding: 5 }}
-                        colors={['#5A189A', '#9C4EDC', '#DFA9FD']}
-                    >
-                        <TouchableOpacity onPress={pickImage} className="">
-                            <View className="rounded-full bg-white p-3">
-                                <View className="rounded-full bg-gray-400">
-                                    {image ? (
-                                        <Image
-                                            source={{ uri: image }}
-                                            style={styles.image}
-                                            className="rounded-full"
-                                        />
-                                    ) : (
-                                        <Ionicons
-                                            name="person"
-                                            size={64}
-                                            color="white"
-                                            className="items-center justify-center p-3"
-                                        />
-                                    )}
+                <SafeAreaView className='w-full flex items-center justify-between'>
+                    <View>
+                        <Text className="text-3xl text-white">Estamos quase lá...</Text>
+                    </View>
+                    <View className="mb-5 mt-10 rounded-full bg-button_color">
+                        <Text className="p-2 text-xl text-white">
+                            Adicione uma foto de perfil!
+                        </Text>
+                    </View>
+                    <View className='relative flex items-center'>
+                        <LinearGradient
+                            style={{ borderRadius: 999, padding: 5 }}
+                            colors={['#5A189A', '#9C4EDC', '#DFA9FD']}
+                        >
+                            <TouchableOpacity onPress={pickImage} className="">
+                                <View className="rounded-full bg-white ">
+                                    <View className="rounded-full bg-gray-400">
+                                        {image ? (
+                                            <Image
+                                                source={{ uri: image }}
+                                                style={styles.image}
+                                                className="rounded-full"
+                                            />
+                                        ) : (
+                                            <Ionicons
+                                                name="person"
+                                                size={64}
+                                                color="white"
+                                                className="items-center justify-center p-3"
+                                            />
+                                        )}
+                                    </View>
                                 </View>
+                            </TouchableOpacity>
+                        </LinearGradient>
+
+                        <TouchableOpacity
+                            onPress={pickImage}
+                            className="absolute top-[85px] left-[85px]"
+                        >
+                            <View className="bg-button_color rounded-full">
+                                <Ionicons name="pencil" size={22} color="white" className="p-2" />
                             </View>
                         </TouchableOpacity>
-                    </LinearGradient>
-                
-                <TouchableOpacity
-                    onPress={pickImage}
-                    className="absolute top-[90%] left-[25%]"
-                >
-                    <View className="bg-button_color rounded-full">
-                        <Ionicons name="pencil" size={22} color="white" className="p-2" />
                     </View>
-                </TouchableOpacity>
-                </View>
-                <View className="w-[80%] gap-5">
-                    <View className="mx-auto mt-14 w-[70%] items-center rounded-full bg-button_color">
-                        <Text className="p-2 text-xl text-white">
-                            Escolha seu @ de usuário!
+                    <View className="w-[80%] gap-5">
+                        <View className="mx-auto mt-14 w-[70%] items-center rounded-full bg-button_color">
+                            <Text className="p-2 text-xl text-white">
+                                Escolha seu @ de usuário!
+                            </Text>
+                        </View>
+                        <RoleInput
+                            type="at"
+                            value={username}
+                            error={usernameError}
+                            onChangeText={handleUsernameChange}
+                        />
+                        <View className="mx-auto items-center rounded-full bg-button_color">
+                            <Text className="p-2 text-xl text-white">
+                                Como seus amigos te chamam?
+                            </Text>
+                        </View>
+                        <RoleInput
+                            type="nickname"
+                            value={nickname}
+                            error={nicknameError}
+                            onChangeText={handleNicknameChange}
+                        />
+                    </View>
+                    <View className="text-end"></View>
+                    <View className="my-6 flex w-[80%]">
+                        <Text className="mb-5 text-center text-2xl text-white">
+                            Preparado(a)?
                         </Text>
+                        <RoleMainButton type="gradient">
+                            <Text className="text-white">BORA</Text>
+                        </RoleMainButton>
                     </View>
-                    <RoleInput
-                        type="at"
-                        value={username}
-                        error={usernameError}
-                        onChangeText={handleUsernameChange}
-                    />
-                    <View className="mx-auto items-center rounded-full bg-button_color">
-                        <Text className="p-2 text-xl text-white">
-                            Como seus amigos te chamam?
-                        </Text>
-                    </View>
-                    <RoleInput
-                        type="nickname"
-                        value={nickname}
-                        error={nicknameError}
-                        onChangeText={handleNicknameChange}
-                    />
-                </View>
-                <View className="text-end"></View>
-                <View className="my-6 flex w-[80%]">
-                    <Text className="mb-5 text-center text-2xl text-white">
-                        Preparado(a)?
-                    </Text>
-                    <RoleMainButton type="gradient">
-                        <Text className="text-white">BORA</Text>
-                    </RoleMainButton>
-                </View>
+                </SafeAreaView>
             </Background>
         </>
     )
