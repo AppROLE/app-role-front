744191fe32d5f3398e33851bf01427e10f2eac9d
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-09-04 19:15:23 -0300

    feat: Add confirmForgotPassword method to AuthRepositoryHttp
    The AuthRepositoryHttp class has been updated to include a new method, confirmForgotPassword, which sends a POST request to the "/confirm-forgot-password" endpoint with the provided data. This enhancement improves the functionality of the AuthRepositoryHttp class by allowing users to confirm their forgotten passwords.

--- api/repositories/auth_repository_http.ts
+++ api/repositories/auth_repository_http.ts
@@ -1,4 +1,5 @@
 import { http } from '../http'
+import { confirmForgotPasswordRequestDTO, confirmForgotPasswordResponseDTO } from '../types/auth_dto'
 
 export class AuthRepositoryHttp {
   async signIn(email: string, password: string) {
@@ -31,6 +32,15 @@ export class AuthRepositoryHttp {
     try {
       const response = await http.post('', data)
       return response.data
+    } catch (error: any) {
+      return error.response.data
+    }
+  }
+
+  async confirmForgotPassword(data: confirmForgotPasswordRequestDTO) {
+    try {
+      const response = await http.post(`/confirm-forgot-password`, data)
+      return response.data as confirmForgotPasswordResponseDTO
     } catch (error: any) {
       return error.response.data
     }
