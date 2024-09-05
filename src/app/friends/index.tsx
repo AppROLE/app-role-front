e6540222e1928375abd53bdae409d28b79c274b3
ZettDev <23.00171-2@maua.br>
2024-09-05 11:09:08 -0300

    friends screen fix

--- src/app/friends/index.tsx
+++ src/app/friends/index.tsx
@@ -1,16 +1,14 @@
 import Background from "@/src/components/background";
 import {Text, TouchableOpacity, View, FlatList} from "react-native";
-import React from "react";
-import Entypo from "@expo/vector-icons/Entypo";
+import { useState } from "react";
 import FriendCard from "@/src/components/friendCard";
 
 export default function Favorites() {
-
     const json = [
         {
             "id": 1,
             "image": "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
-            "nickname": "Jõao",
+            "nickname": "João",
             "user": "joao123"
         },
         {
@@ -33,18 +31,17 @@ export default function Favorites() {
         }
     ]
 
+    const [friends, setFriends] = useState(json)
+
     return (
         <Background>
             <View className="items-start h-full w-full">
                 <View className='flex-row justify-between w-full px-8'>
-                    <Text className="text-3xl text-white font-medium">Social</Text>
-                    <View className='flex-row items-center gap-4'>
-
-                    </View>
+                    {/* BARRA DE PESQUISA E BOTÃO DE VOLTAR */}
                 </View>
                 <View className="border-t-line_gray border-2 mt-8 p-10 w-full">
                     <FlatList
-                        data={json}
+                        data={friends}
                         keyExtractor={(item) => item.id.toString()}
                         renderItem={
                             ({item}) => <FriendCard image={item.image} nickname={item.nickname}
