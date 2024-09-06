9d27ef4a23d3dfa8a70062b55ff23b5697599995
João <galhardojoaopedro@gmail.com>
2024-09-05 23:54:29 -0300

    feat: Add finishSignUp and uploadImageProfile methods to AuthRepositoryHttp

--- api/repositories/auth_repository_http.ts
+++ api/repositories/auth_repository_http.ts
@@ -1,4 +1,7 @@
+import { router } from 'expo-router';
 import { http } from '../http'
+import { finishSignUpRequestDTO } from '../types/auth_dto'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export class AuthRepositoryHttp {
   async signIn(email: string, password: string) {
@@ -33,6 +36,42 @@ export class AuthRepositoryHttp {
       return response.data
     } catch (error: any) {
       return error.response.data
+    }
+  }
+
+  async finishSignUp(data: object) {
+    try {
+      const response = await http.post('/finish-sign-up', data)
+      
+      if (response?.status === 201) {
+        router.replace('/home');
+      }
+      if (response?.status === 409) { 
+        alert('Usuário já cadastrado');
+      }
+
+      console.log("RESPOSTA DA REQ FINISH SIGN UP");
+      console.log(response.data);
+
+      return response.data as finishSignUpRequestDTO;
+    } catch (error: any) {
+      console.log(error)
+      return error.response.data
+    }
+  }
+
+  async uploadImageProfile(formData: FormData) {
+    try {
+      const response = await http.post('/upload-image-profile', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      console.log("RESPOSTA DA REQ IMAGE PROFILE" + response);
+      return response;
+    } catch (error: any) {
+      console.log(error);
+      return error.response.data;
     }
   }
 }
