90e60837507259318155caabff081f85768b6d8e
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-09-18 18:14:34 -0300

    privacy init

---
+++ src/app/privacy/index.tsx
@@ -0,0 +1,20 @@
+import {
+    View,
+} from 'react-native';
+import Background from '@/src/components/background';
+import React from "react";
+import BigButton from "@/src/components/bigButton";
+import Svg from "@/src/components/svg";
+
+export default function Privacy() {
+
+    return (
+        <Background>
+            <View className="w-full flex-1">
+                <View className="flex flex-row h-12 w-full items-center gap-3 border-b-2 border-b-line_gray">
+
+                </View>
+            </View>
+        </Background>
+    );
+}
