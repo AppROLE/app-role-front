e61420bfe6e2301c943314163a18ee27ad685846
João <galhardojoaopedro@gmail.com>
2024-09-19 10:45:30 -0300

    feat: Remove create method from InstituteRepositoryHttp
    The create method has been removed from InstituteRepositoryHttp as it is not being used.

--- api/repositories/institute_repository_http.ts
+++ api/repositories/institute_repository_http.ts
@@ -2,15 +2,6 @@ import { http } from "../http"
 
 
 export class InstituteRepositoryHttp {
-  async create(data: object) {
-    try {
-      const response = await http.post('/institute', data)
-      return response.data
-    } catch (error: any) {
-      return error.response.data
-    }
-  }
-
   async getAll() { 
     try {
       const response = await http.get('/get-all-institutes')
