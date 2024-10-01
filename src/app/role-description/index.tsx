f6e04acba3625d4174ae172e3862a2e174831017
João <galhardojoaopedro@gmail.com>
2024-10-01 14:42:13 -0300

    Refactor RoleDescription component to include a modal for displaying a list of confirmed users and avatars

--- src/app/role-description/index.tsx
+++ src/app/role-description/index.tsx
@@ -21,57 +21,59 @@ export default function RoleDescription() {
         { id: 3, uri: require('../../../assets/images/profile3 (1).png') },
     ];
 
+    const maxParticipantsToShow = 3;
+
     return (
         <>
             <Background>
                 <SafeAreaView>
-                        <TouchableOpacity onPress={() => setConfirmedListModalVisible(true)}>
-                            <View className="flex items-center mb-12">
-                                {/* Lista de Avatares */}
-                                <View className="flex-row space-x-2">
-                                    {avatars.map((avatar) => (
-                                        <Image
-                                            key={avatar.id}
-                                            source={avatar.uri}
-                                            className="w-12 h-12 rounded-full"
-                                        />
-                                    ))}
-                                    {/* Botão de Adicionar */}
-                                    <View className="w-12 h-12 rounded-full bg-[#333] flex items-center justify-center">
-                                        <Ionicons name="add" size={20} color="#fff" />
-                                    </View>
+                    <TouchableOpacity onPress={() => setConfirmedListModalVisible(true)} className="w-full self-end">
+                        <View className="flex items-center mb-12 ">
+                            <View className="flex-row absolute justify-center items-center">
+                                {avatars.slice(0, maxParticipantsToShow).map((avatar) => (
+                                    <Image
+                                        key={avatar.id}
+                                        source={avatar.uri}
+                                        className="w-14 h-14 rounded-full relative -ml-4 "
+                                    />
+                                ))}
+                                <View className="w-8 h-8 ml-4 rounded-full bg-[#333] absolute left-[100px] flex items-center justify-center">
+                                    <Ionicons name="add" size={18} color="#fff"  />
                                 </View>
-                                <Text className="text-[#DFA9FD] text-sm mt-5">Lista de confirmados</Text>
                             </View>
-                        </TouchableOpacity>
-                        <ComfirmedListModal
-                            visible={comfirmedListModalVisible}
-                            onClose={() => setConfirmedListModalVisible(false)}
-                        />
-                        <View>
-                            <TouchableOpacity className="flex-row items-center bg-[#1C1C1C] rounded-full px-4 py-2" onPress={() => setModalVisible(true)}>
-                                <View className="flex-row">
-                                    {stars.map((star) => (
-                                        <Ionicons
-                                            name='star-outline'
-                                            size={20}
-                                            color="#fff"
-                                            className="mr-1"
-                                            key={star}
-                                        />
-
-                                    ))}
-                                </View>
-                                <View className="w-px h-6 bg-white mx-3"></View>
-                                <Text className="text-sm text-white">
-                                    Deixe sua Avaliação
-                                </Text>
-                            </TouchableOpacity>
+                            <View className="mt-12">
+                                <Text className="text-[#DFA9FD] text-md mt-5">Lista de confirmados</Text>
+                            </View>
                         </View>
-                        <ModalReview
-                            visible={modalVisible}
-                            onClose={() => setModalVisible(false)}
-                        />
+                    </TouchableOpacity>
+                    <ComfirmedListModal
+                        visible={comfirmedListModalVisible}
+                        onClose={() => setConfirmedListModalVisible(false)}
+                    />
+                    <View>
+                        <TouchableOpacity className="flex-row items-center bg-[#1C1C1C] rounded-full px-4 py-2" onPress={() => setModalVisible(true)}>
+                            <View className="flex-row">
+                                {stars.map((star) => (
+                                    <Ionicons
+                                        name='star-outline'
+                                        size={20}
+                                        color="#fff"
+                                        className="mr-1"
+                                        key={star}
+                                    />
+
+                                ))}
+                            </View>
+                            <View className="w-px h-6 bg-white mx-3"></View>
+                            <Text className="text-sm text-white">
+                                Deixe sua Avaliação
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <ModalReview
+                        visible={modalVisible}
+                        onClose={() => setModalVisible(false)}
+                    />
                 </SafeAreaView>
             </Background>
         </>
