da0b9337e6d02270d8bd75108962d3848616c986
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-10-02 18:49:03 -0300

    Add httpEvent axios instance for event API

--- api/http.ts
+++ api/http.ts
@@ -3,3 +3,7 @@ import axios from 'axios'
 export const http = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_AUTH_URL
 })
+
+export const httpEvent = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_EVENT_URL
+})
