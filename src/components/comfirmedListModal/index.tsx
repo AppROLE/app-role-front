c238198b33a278b90b2920bd1a4a8e7b526dc919
João <galhardojoaopedro@gmail.com>
2024-10-02 16:06:13 -0300

    Refactor ComfirmedListModal component to use presence and friends data
    This commit refactors the ComfirmedListModal component to use the presence and friends data retrieved from the server. It replaces the previous hardcoded data with the actual data fetched from the API. The component now uses the PresenceContext to get the presence data and the AuthContext to get the friends data. The presence data is mapped to include additional information such as the profile photo and whether the user is a friend. The friends data is used to determine if a user is a friend or not. This improves the accuracy and relevance of the displayed information in the ComfirmedListModal component.

--- src/components/comfirmedListModal/index.tsx
+++ src/components/comfirmedListModal/index.tsx
@@ -1,11 +1,13 @@
-import { getAllConfirmedUsersResponseDTO, Users } from "@/api/types/event_dto";
+import { Presence } from "@/api/types/presence_dto";
 import { ImageSourcePropType } from "react-native";
-import { EventContext } from "@/context/event_context";
 import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useContext, useEffect, useState } from "react";
-import { Keyboard, Modal, Text, TouchableOpacity, TouchableWithoutFeedback, View, Image } from "react-native";
+import { Keyboard, Modal, Text, TouchableOpacity, View, Image } from "react-native";
 import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
+import { PresenceContext } from "@/context/presence_context";
+import { AuthContext } from "@/context/auth_context";
+import { Friends } from "@/api/types/auth_dto";
 
 interface ModalReviewProps {
     visible: boolean;
@@ -13,27 +15,51 @@ interface ModalReviewProps {
 }
 
 export default function ComfirmedListModal({ visible, onClose }: ModalReviewProps) {
-    const [users, setUsers] = useState<(Users & { image: ImageSourcePropType })[]>([]);
-    const { getAllConfirmedUsers } = useContext(EventContext);  
+    const [presence, setPresence] = useState<(Presence & {profilePhoto?: ImageSourcePropType})[]>([]);
+    const [friends, setFriends] = useState<(Friends)[]>([]);
+    const { getAllPresence} = useContext(PresenceContext);  
+    const { getFriends } = useContext(AuthContext);
 
-    const comfirmed = [
-        { event_id: 1, iamge: require('../../../assets/images/profile1 (1).png'), username: 'João', nickname: 'jotape', comfirmed: true },
-        { event_id: 2, image: require('../../../assets/images/profile2 (1).png'), username: 'Gabriel', nickname: 'gabs', comfirmed: true },
-        { event_id: 3, image: require('../../../assets/images/profile3 (1).png'), username: 'Rodrigo', nickname: 'digos', comfirmed: false }
-    ]
+    // const comfirmed = [
+    //     { event_id: 1, iamge: require('../../../assets/images/profile1 (1).png'), username: 'João', nickname: 'jotape', comfirmed: true },
+    //     { event_id: 2, image: require('../../../assets/images/profile2 (1).png'), username: 'Gabriel', nickname: 'gabs', comfirmed: true },
+    //     { event_id: 3, image: require('../../../assets/images/profile3 (1).png'), username: 'Rodrigo', nickname: 'digos', comfirmed: false }
+    // ]
+
+    async function get() {
+        const idToken = (await AsyncStorage.getItem('idToken')) || ''
+        if (idToken === '') return;
+        const response = await getFriends(idToken);
+        if (response) {
+            setFriends(response.friends);
+        } 
+    }
+
+    async function fetchAllConfirmedUsers() {
+        const idToken = (await AsyncStorage.getItem('idToken')) || ''
+        if (idToken === '') return;
+        const eventId = (await AsyncStorage.getItem('eventId')) || ''
+        if (eventId === '') return;
+
+        const response = await getAllPresence(idToken, eventId);
+        if (response) {
+            setPresence(response.users);
+        }
+    }
 
     useEffect(() => {
-        async function fetchAllConfirmedUsers() {
-            const idToken = (await AsyncStorage.getItem('idToken')) || ''
-            if (idToken === '') return;
-            const response = await getAllConfirmedUsers(idToken);
-            if (response) {
-                setUsers(response.users);
-            }
-        }
         fetchAllConfirmedUsers();
+        get();
     }, [])
 
+    const confirmeds = presence.map(user => {
+        const isFriend = friends.some(friend => friend.username === user.username);
+
+        return {
+            ...user,
+            isFriend,
+        };
+    });
 
     return (
         <>
@@ -56,14 +82,14 @@ export default function ComfirmedListModal({ visible, onClose }: ModalReviewProp
                             </View>
                         </View>
                         <ScrollView className="w-full mt-8">
-                            {comfirmed.map((user) => (
+                            {confirmeds.map((user) => (
                                 <View className="w-full bg-button_color mt-5 rounded-full" key={user.event_id}>
                                     <View
                                         className="w-full flex-row items-center justify-between rounded-full p-2"
                                     >
                                         <Image
-                                            source={user.image}
-                                            className={`w-16 h-16 rounded-full mr-4 ${user.comfirmed ? 'border-2 border-green-500' : ''}`}
+                                            source={user.profilePhoto as ImageSourcePropType}
+                                            className={`w-16 h-16 rounded-full mr-4 ${user.isFriend ? 'border-2 border-green-500' : ''}`}
                                             resizeMode="cover"
                                         />
                                         <View className="flex-1">
