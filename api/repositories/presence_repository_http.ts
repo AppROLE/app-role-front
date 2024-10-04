98276b95111db684e6bd5fa7e11dc61f3ddbe0c4
João <galhardojoaopedro@gmail.com>
2024-10-03 21:43:07 -0300

    Refactor PresenceRepositoryHttp to remove unnecessary parameter and retrieve idToken from AsyncStorage

--- api/repositories/presence_repository_http.ts
+++ api/repositories/presence_repository_http.ts
@@ -1,22 +1,24 @@
 import Toast from "react-native-toast-message";
 import { http } from "../http";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 export class PresenceRepositoryHttp {
-    async getAllPresences(idToken: string, eventId: string) { 
+    async getAllPresences(eventId: string) { 
         try {
+            const idToken = await AsyncStorage.getItem('idToken') || ''
+            if (idToken === '') return;
             const response = await http.get(`/get-all-presences?eventId=${eventId}`, {
                 headers: {
                     Authorization: `Bearer ${idToken}` 
                 }
             });
-            if (response.status === 200 && response.data.message === 'Nenhuma presença encontrada') {
-                alert(response.data.message)
-                return []
-            }
+            console.log("RESPOSTA DA REQUEST", response)
             return response.data
         } catch (error: any) {
-            return error.response.data
+            console.log("ERRO NA REQUEST", error.response.data)
+            alert(error.response.data.message)
+            return error.response.data.message
         }
     }
 }
