43b9f928f5834d8386e48386433e6e6597885517
João <galhardojoaopedro@gmail.com>
2024-10-04 02:02:55 -0300

    Refactor PresenceContextProvider to add confirmEvent method

--- context/presence_context.tsx
+++ context/presence_context.tsx
@@ -1,17 +1,23 @@
 import { PresenceRepositoryHttp } from "@/api/repositories/presence_repository_http"
-import { getAllConfirmedUsersResponseDTO } from "@/api/types/presence_dto"
+import { confirmEventResponseDTO, getAllConfirmedUsersResponseDTO } from "@/api/types/presence_dto"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { createContext } from "react"
 
 
 type PresenceContextType = {
     getAllPresence: (eventId: string) => Promise<getAllConfirmedUsersResponseDTO>
+    confirmEvent: (eventId: string, profilePhoto?: string, promoterCode?: string) => Promise<confirmEventResponseDTO>
 }
 
 const defaultPresenceContext = { 
     getAllPresence: async () => {
         return {
             users: [],
+            message: ''
+        }
+    },
+    confirmEvent: async () => {
+        return {
             message: ''
         }
     }
@@ -32,8 +38,17 @@ export function PresenceContextProvider({ children }: any) {
         }
     }
 
+    async function confirmEvent(eventId: string, profilePhoto?: string, promoterCode?: string) { 
+        try {
+            const response = await presenceRepository.confirmEvent(eventId, profilePhoto, promoterCode)
+            return response
+        } catch (error: any) {
+            return error
+        }
+    }
+
     return (
-        <PresenceContext.Provider value={{ getAllPresence }}>
+        <PresenceContext.Provider value={{ getAllPresence, confirmEvent }}>
             {children}
         </PresenceContext.Provider>
     )
