348023a1be532c3e9d06b1ce3e77a3c1f691e107
João <galhardojoaopedro@gmail.com>
2024-10-03 23:58:10 -0300

    Refactor code to update font styles in the ConfirmForgotPassword component and add font styles to text elements in the EditingPerfil component

--- src/app/sign-in/index.tsx
+++ src/app/sign-in/index.tsx
@@ -72,12 +72,12 @@ export default function Index() {
 
   return (
     <Background>
-      <View className="w-full flex-1">
+      <View className="w-full flex-1 ">
         <View className="flex flex-col justify-center px-[8%]">
-          <Text className="text-center text-xl text-white">
-            Já está com saudade de um <Text className="font-bold">ROLE</Text>?
+          <Text className="text-center text-xl text-white font-sans">
+            Já está com saudade de um <Text className="font-sansBold">ROLE</Text>?
           </Text>
-          <Text className="text-center text-xl font-bold text-white">
+          <Text className="text-center text-xl font-sansBold text-white">
             Bem vindo(a) de volta!
           </Text>
         </View>
@@ -96,8 +96,8 @@ export default function Index() {
               error={passwordError}
             />
             <View className="flex flex-row gap-2">
-              <Text className="text-xs text-white">Esqueceu sua senha?</Text>
-              <Link className="text-xs text-[#D8A9FF]" href="/forgot-password">
+              <Text className="text-xs text-white font-sans">Esqueceu sua senha?</Text>
+              <Link className="text-xs text-[#D8A9FF] font-sans" href="/forgot-password">
                 Recuperar senha
               </Link>
             </View>
@@ -105,18 +105,18 @@ export default function Index() {
         </View>
         <View className="gap-12 px-[8%]">
           <RoleMainButton type="gradient" buttonFunction={Login}>
-            <Text className="text-white">Entrar</Text>
+            <Text className="text-white font-sans">Entrar</Text>
           </RoleMainButton>
           <RoleMainButton type="simple" buttonFunction={() => promptAsyncGoogle()}>
             <FontAwesome6 name="google" size={24} color="white" />
-            <Text className="text-white">Entrar via Google</Text>
+            <Text className="text-white font-sans">Entrar via Google</Text>
           </RoleMainButton>
         </View>
         <View className="flex-grow" />
         <View className="mb-16 py-4">
           <View className="mx-auto flex flex-row justify-center gap-2">
-            <Text className="text-[#BDBDBD]">Não possui uma conta?</Text>
-            <Link className="text-[#D8A9FF]" href="/sign-up">
+            <Text className="text-[#BDBDBD] font-sans">Não possui uma conta?</Text>
+            <Link className="text-[#D8A9FF] font-sans" href="/sign-up">
               Criar Conta
             </Link>
           </View>
