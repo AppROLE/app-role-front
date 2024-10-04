689e9a538b68db1f3f6641e5e71d9cc12768b061
João <galhardojoaopedro@gmail.com>
2024-10-04 03:29:09 -0300

    Refactor ReviewRepositoryHttp to add getAllReviewsByEvent method

--- api/types/review_dto.ts
+++ api/types/review_dto.ts
@@ -1,11 +1,21 @@
 
 export type createReviewRequestDTO = { 
-    instituteId: string
-    reviewedAt: number
-    star: number
-    review: string
+    instituteId: string;
+    reviewedAt: number;
+    star: number;
+    review: string;
 }
 
 export type createReviewResponseDTO = {
-    message: string
+    message: string;
 }
+
+export type getAllReviewsByEventResponseDTO = {
+    instituteId: string;
+    eventId: string;
+    nickname: string;
+    reviewedAt: number;
+    star: number;
+    review: string;
+    profilePhoto: string;
+}[]
