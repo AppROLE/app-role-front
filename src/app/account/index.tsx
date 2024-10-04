db04083d1895d4f4190a0b0222000f59ff84b0e4
João <galhardojoaopedro@gmail.com>
2024-10-03 23:55:01 -0300

    Refactor code to add SafeAreaView and load fonts

--- src/app/account/index.tsx
+++ src/app/account/index.tsx
@@ -56,7 +56,7 @@ export default function Account() {
             />
           </TouchableOpacity>
           <View className="flex-1 h-full mb-8">
-            <Text className="absolute left-1/2 transform -translate-x-1/2 text-white text-3xl font-bold">
+            <Text className="absolute left-1/2 transform -translate-x-1/2 text-white text-3xl font-sansBold">
               Conta
             </Text>
           </View>
@@ -64,7 +64,7 @@ export default function Account() {
 
         <ScrollView>
           <View className="pl-6 gap-6 pt-6 items-start border-b-2 border-b-line_gray">
-            <Text className="text-white text-lg font-bold">
+            <Text className="text-white text-lg font-sansBold">
               Data de nascimento
             </Text>
             <TouchableOpacity className="bg-button_color p-2 justify-center rounded-lg mb-8">
@@ -74,55 +74,55 @@ export default function Account() {
             </TouchableOpacity>
           </View>
           <View className="px-6 gap-6 pt-6 items-start border-b-2 border-b-line_gray">
-            <Text className="text-white text-lg font-bold">
+            <Text className="text-white text-lg font-sansBold">
               Telefone
             </Text>
             <RoleInput type="none" placeholder="(00) 0 0000-0000" value={phone} onChangeText={handlePhoneChange}/>
           </View>
           <View className="px-6 gap-6 pt-6 items-start border-b-2 border-b-line_gray">
-            <Text className="text-white text-lg font-bold">
+            <Text className="text-white text-lg font-sansBold">
               CPF
             </Text>
             <RoleInput type="none" placeholder="000.000.000-00" value={cpf} onChangeText={handleCpfChange}/>
           </View>
           <View className="px-6 gap-6 pt-6 items-start border-b-2 border-b-line_gray">
-            <Text className="text-white text-lg font-bold">
+            <Text className="text-white text-lg font-sansBold">
               Gênero
             </Text>
             <View className="flex flex-row gap-2">
               <TouchableOpacity className="bg-button_color justify-center p-2 rounded-lg mb-8">
-                <Text className="text-sub_text text-lg">
+                <Text className="text-sub_text text-lg font-sans">
                   Masculino
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity className="bg-button_color justify-center p-2 rounded-lg mb-8">
-                <Text className="text-sub_text text-lg">
+                <Text className="text-sub_text text-lg font-sans">
                   Feminino
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity className="bg-button_color justify-center p-2 rounded-lg mb-8">
-                <Text className="text-sub_text text-lg">
+                <Text className="text-sub_text text-lg font-sans">
                   Outro
                 </Text>
               </TouchableOpacity>
             </View>
           </View>
           <View className="gap-6 pt-6 items-start border-b-2 border-b-line_gray">
-            <Text className="ml-6 text-white text-lg font-bold">
+            <Text className="ml-6 text-white text-lg font-sansBold">
               E-mail
             </Text>
             <TouchableOpacity onPress={openChange} className="bg-button_color p-2 justify-center self-center rounded-3xl mb-8 w-10/12">
-              <Text className="text-white text-lg text-center">
+              <Text className="text-white text-lg text-center font-sans">
                 Alterar E-mail
               </Text>
             </TouchableOpacity>
           </View>
           <View className="gap-6 pt-6 items-start">
-            <Text className="ml-6 text-white text-lg font-bold">
+            <Text className="ml-6 text-white text-lg font-sansBold">
               Senha
             </Text>
             <TouchableOpacity onPress={openChange} className="bg-button_color p-2 justify-center self-center rounded-3xl mb-8 w-10/12">
-              <Text className="text-white text-lg text-center">
+              <Text className="text-white text-lg text-center font-sans">
                 Alterar Senha
               </Text>
             </TouchableOpacity>
@@ -130,7 +130,7 @@ export default function Account() {
         </ScrollView>
         <View onLayout={handleLayout} className="absolute flex justify-end items-center py-4 bg-button_color w-full" style={{bottom: tabBarHeight}}>
           <BigButton buttonFunction={onSave}>
-            <Text className="text-white text-lg">
+            <Text className="text-white text-lg font-sans">
               Salvar
             </Text>
           </BigButton>
