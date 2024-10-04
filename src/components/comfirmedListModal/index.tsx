472202d9da3cf8b7cf5ffc82c235b6cbff8e4137
João <galhardojoaopedro@gmail.com>
2024-10-04 03:29:48 -0300

    Refactor ComfirmedListModal component to update styling for the background and dimensions

--- src/components/comfirmedListModal/index.tsx
+++ src/components/comfirmedListModal/index.tsx
@@ -65,7 +65,7 @@ export default function ComfirmedListModal({ visible, onClose, eventId }: ModalR
                 onRequestClose={onClose}
             >
                 <View className="flex-1 justify-end items-center bg-black/70">
-                    <View className="flex-end bg-background rounded-t-[40px] p-3 h-[89%] w-full">
+                    <View className="flex-end bg-background rounded-t-[50px] p-3 h-[695px] w-full">
                         <View className="flex-row items-center justify-center pt-[15px]">
                             <TouchableOpacity className="rounded-full bg-button_color mr-5 absolute left-[10px] top-[12px]" onPress={onClose}>
                                 <Ionicons name="arrow-back" size={22} color='#fff' className="p-2" />
