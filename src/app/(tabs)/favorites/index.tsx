0b6b74acff07185b0530f61cccbffe747a995d0a
João <galhardojoaopedro@gmail.com>
2024-10-14 13:25:47 -0300

    Refactor favorites screen and add getAllFavoritesInstitutes method

--- src/app/(tabs)/favorites/index.tsx
+++ src/app/(tabs)/favorites/index.tsx
@@ -1,14 +1,19 @@
 import Background from "@/src/components/background";
 import {Text, TouchableOpacity, View, FlatList, Dimensions} from "react-native";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import SocialCard from "@/src/components/socialCard";
 import Svg from "@/src/components/svg";
 
 import { router } from 'expo-router'
+import { InstituteContext } from "@/context/institute_context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Institute } from "@/api/types/institute_dto";
 
 
 export default function Favorites() {
     const { width } = Dimensions.get('window')
+    const { getAllFavoritesInstitutes } = useContext(InstituteContext)
+    const [institutes, setInstitutes] = useState<Institute[]>([])
 
     const json = [
         {
@@ -35,6 +40,25 @@ export default function Favorites() {
     function navigateToNotifications() {
         router.navigate('/notifications')
     }
+
+    async function getAll() {
+        try {
+            const idToken = await AsyncStorage.getItem('idToken') || ''
+            if (idToken === '') return;
+            const response = await getAllFavoritesInstitutes(idToken);
+            if (response) {
+                console.log('RESPOSTA DA GET ALL', response) 
+                setInstitutes(response.institutes)
+            }
+            return response
+        } catch (error: any) {
+            return error
+        }
+    }
+
+    useEffect(() => { 
+        getAll();
+    }, []);
 
     return (
         <Background>
@@ -70,10 +94,10 @@ export default function Favorites() {
                 <View className="border-t-line_gray border-t-2 mt-8 p-10 w-full">
                     <Text className="text-2xl text-white mb-3">Instituições Favoritas</Text>
                     <FlatList
-                        data={json}
-                        keyExtractor={(item) => item.id.toString()}
+                        data={institutes}
+                        keyExtractor={(institute) => institute.instituteId}
                         renderItem={
-                            ({item}) => <SocialCard title={item.title} image={item.image}/>
+                            ({ item: institute }) => <SocialCard title={institute.name} image={institute.logoPhoto}/>
                         }
                     />
                 </View>
