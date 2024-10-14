b93e819c6ee731e01385391cd088dd7d6f612fcf
João <galhardojoaopedro@gmail.com>
2024-10-14 13:25:53 -0300

    Refactor profile screen and add getAllConfirmedEvents method

--- src/app/(tabs)/profile/index.tsx
+++ src/app/(tabs)/profile/index.tsx
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { View, Text, Image, TouchableOpacity, Linking, Alert } from "react-native";
 import { SvgUri } from "react-native-svg";
 import MenuHamburguer from "@/src/components/menuHamburguer";
 import RoleMainButton from "@/src/components/roleMainButton";
 import { useRouter } from "expo-router";
 import { UserRepositoryHttp } from "@/api/repositories/user_repository_http"; // ajuste o caminho conforme necessário
+import RoleCard from "@/src/components/roleCard";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { PresenceContext } from "@/context/presence_context";
+import { Events } from "@/api/types/presence_dto";
 
 export default function Profile() {
   const navigation = useRouter();
-  
+
   // Instanciar a classe UserRepositoryHttp
   const userRepository = new UserRepositoryHttp();
-  
+
   // Estados para armazenar os dados do perfil
   const [profileData, setProfileData] = useState({
     user_id: '',
@@ -64,11 +68,28 @@ export default function Profile() {
     }
   }
 
+  const [roles, setRoles] = useState<Events[]>([]);
+  const { getAllConfirmedEvents } = useContext(PresenceContext);
+
+  async function fetchRoles() {
+    const idToken = await AsyncStorage.getItem('idToken') || '';
+    if (idToken === '') return;
+    const response = await getAllConfirmedEvents(idToken);
+    console.log('RESPOSTA DA GET ALL', response);
+    if (response) {
+      setRoles(response.events);
+    }
+  }
+
+  useEffect(() => {
+    fetchRoles();
+  }, []);
+
   return (
     <View className="bg-[#999999] h-full">
       <Image
         source={{ uri: profileData.backgroundPhoto || process.env.EXPO_PUBLIC_URL_S3 + '/images/background_default.png' }}
-        className="h-[215px] w-full"/>
+        className="h-[215px] w-full" />
       <MenuHamburguer />
       <View className="absolute bottom-0 bg-[#121212] h-[75%] w-full flex flex-col items-center rounded-t-[54px] pt-12">
         <View className="absolute -top-10 bg-[#121212] w-28 h-28 rounded-full overflow-hidden items-center justify-center">
@@ -99,39 +120,55 @@ export default function Profile() {
             />
             <Text className="text-white">Editar Perfil</Text>
           </RoleMainButton>
-          <View 
+          <View
             className="bg-[#1C1C1C] flex flex-row gap-6"
-            style={{paddingVertical: 8, paddingHorizontal: 30, borderRadius: 20}}>
-              <TouchableOpacity onPress={() => openLink(profileData.linkTiktok)}>
-                <SvgUri
-                  uri={process.env.EXPO_PUBLIC_URL_S3 + '/tiktok.svg'}
-                  width={30}
-                  height={30}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity className="top-[2px]" onPress={() => openLink(profileData.linkInstagram)}>
-                <SvgUri
-                  uri={process.env.EXPO_PUBLIC_URL_S3 + '/instagram.svg'}
-                  width={26}
-                  height={26}
-                />
-              </TouchableOpacity>
+            style={{ paddingVertical: 8, paddingHorizontal: 30, borderRadius: 20 }}>
+            <TouchableOpacity onPress={() => openLink(profileData.linkTiktok)}>
+              <SvgUri
+                uri={process.env.EXPO_PUBLIC_URL_S3 + '/tiktok.svg'}
+                width={30}
+                height={30}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity className="top-[2px]" onPress={() => openLink(profileData.linkInstagram)}>
+              <SvgUri
+                uri={process.env.EXPO_PUBLIC_URL_S3 + '/instagram.svg'}
+                width={26}
+                height={26}
+              />
+            </TouchableOpacity>
           </View>
         </View>
         <View className="mt-8 border-b-2 border-[#DFA9FD]">
           <Text className="text-[#DFA9FD]">ROLEs Confirmados</Text>
         </View>
-        <View className="mt-8">
-          <Text className="text-white">Você ainda não possui um ROLE confirmado :(</Text>
-          <Text className="text-white text-center mt-1">
-            Bora dar um <Text className="text-[#DFA9FD]">ROLE</Text>?
-          </Text>
-        </View>
-        <View className="mt-6">
-          <RoleMainButton type="gradient" buttonFunction={handleProcurar}>
-            <Text className="text-white">Procurar</Text>
-          </RoleMainButton>
-        </View>
+        {roles.slice(0, 5).map((role, index) => (
+          <RoleCard
+            data={role.eventDate}
+            image={role.eventPhotoLink}
+            title={role.name}
+            type={role.category}
+            local={role.address}
+            key={`id${role.eventId}ind${index}`}
+            {...role}
+          />
+        ))}
+
+        {roles.length === 0 && (
+          <>
+            <View className="mt-8">
+              <Text className="text-white">Você ainda não possui um ROLE confirmado :(</Text>
+              <Text className="text-white text-center mt-1">
+                Bora dar um <Text className="text-[#DFA9FD]">ROLE</Text>?
+              </Text>
+            </View>
+            <View className="mt-6">
+              <RoleMainButton type="gradient" buttonFunction={handleProcurar}>
+                <Text className="text-white">Procurar</Text>
+              </RoleMainButton>
+            </View>
+          </>
+        )}
       </View>
     </View>
   );
