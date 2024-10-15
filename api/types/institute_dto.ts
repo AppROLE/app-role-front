16632a271c50582e5ba73be9784418aec80620d2
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-10-14 23:50:06 -0300

    Refactor InstituteRepositoryHttp to use httpEvent for fetching institute by ID

--- api/types/institute_dto.ts
+++ api/types/institute_dto.ts
@@ -8,10 +8,25 @@ export type Institute = {
 
 export type getAllInstituteByIdResponseDTO = { 
     institutes: Institute[],
-    message: string    
+    message: string
 }
 
 export type getInstituteByPartnerTypeResponseDTO = { 
     institutes: Institute[],
     message: string    
+}
+
+export type getInstituteByIdResponseDTO = {
+    addres: string;
+    description: string;
+    district_id: string;
+    events_id: Array<string>;
+    institute_id: string;
+    institute_type: string;
+    logo_photo: string;
+    name: string;
+    partner_type: string;
+    phone: string;
+    photos_url: Array<string>;
+    price: number;
 }
