1b6a0a0f5e6d3a6bb4e73ef34e72412964d1ff97
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-10-15 18:21:53 -0300

    removed log

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -37,8 +37,6 @@ export default function TabLayout() {
         }
     }
 
-    console.log(profileData)
-
     // useEffect para carregar os dados quando o componente for montado
     useEffect(() => {
         fetchProfile();
