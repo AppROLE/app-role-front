9ea0642bc577ecbf8baffe756a9097391db0a7d4
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-10-14 23:50:19 -0300

    Refactor layout to include EventContext and InstituteContext providers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,93 +2,99 @@ import { Slot, Stack } from 'expo-router'
 import '../styles/global.css'
 import { AuthContextProvider } from '@/context/auth_context'
 import ReviewContextProvider from '@/context/review_context'
+import { EventContextProvider } from '@/context/event_context'
+import { InstituteContextProvider } from '@/context/institute_context'
 
 export default function RootLayout() {
   return (
     <AuthContextProvider>
       <ReviewContextProvider>
-        <Stack>
-          <Stack.Screen
-            name="sign-in/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="sign-up/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="opening/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="almost-there/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="create-new-password/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="forgot-password/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="recovery-code/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="confirm-forgot-password/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="friends/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="notifications/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="searching-filters/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="(tabs)"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="first-page/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="editing-perfil/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="configs/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="configs/delete/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="role-description/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="privacy/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="account/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="event-description/index"
-            options={{ headerShown: false }}
-          />
-        </Stack>
+        <EventContextProvider>
+          <InstituteContextProvider>
+            <Stack>
+              <Stack.Screen
+                name="sign-in/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="sign-up/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="opening/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="almost-there/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="create-new-password/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="forgot-password/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="recovery-code/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="confirm-forgot-password/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="friends/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="notifications/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="searching-filters/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="(tabs)"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="first-page/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="editing-perfil/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="configs/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="configs/delete/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="role-description/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="privacy/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="account/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="event-description/index"
+                options={{ headerShown: false }}
+              />
+            </Stack>
+          </InstituteContextProvider>
+        </EventContextProvider>
       </ReviewContextProvider>
     </AuthContextProvider>
   )
