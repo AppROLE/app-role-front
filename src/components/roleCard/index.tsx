a2ab9d9a5c352cfe6516d219b575ce998c9bbacb
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-10-16 12:07:38 -0300

    Refactor RoleCard component to include event ID as a parameter and improve date formatting

--- src/components/roleCard/index.tsx
+++ src/components/roleCard/index.tsx
@@ -1,54 +1,104 @@
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
+import { useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, Image, Dimensions } from "react-native";
 
-interface RoleCardProps {
-    data: string;
-    image: string;
-    title: string;
-    type: string;
-    stars: number;
-    local: string;
-    idRole: string;
-}
-
-export default function RoleCard({data, image, title, type, stars, local, idRole} : RoleCardProps) {
+export default function RoleCard(role: any) {
     const {width, height} = Dimensions.get('window');
+    const [date, setDate] = useState(new Date(role.eventDate));
+    const [loading, setLoading] = useState(false);
     
-    function handleRole() {
+    async function handleRole() {
+        await AsyncStorage.setItem('eventId', role.eventId)
         router.push(`/event-description`)
-        // router.replace('/')
     }
+
+    function dateFormat() {
+        const day = date.getDate();
+        const month = date.getMonth();
+        switch (month) {
+            case 0:
+                return `${day} JAN`
+            case 1:
+                return `${day} FEV`
+            case 2:
+                return `${day} MAR`
+            case 3:
+                return `${day} ABR`
+            case 4:
+                return `${day} MAI`
+            case 5:
+                return `${day} JUN`
+            case 6:
+                return `${day} JUL`
+            case 7:
+                return `${day} AGO`
+            case 8:
+                return `${day} SET`
+            case 9:
+                return `${day} OUT`
+            case 10:
+                return `${day} NOV`
+            case 11:
+                return `${day} DEZ`
+        }
+    }
+
+    function imageValidation() {
+        if (role.eventPhotoLink != '' && role.eventPhotoLink != null && role.eventPhotoLink != undefined) {
+            return role.eventPhotoLink
+        }
+        return 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png'
+    }
+
+    useEffect(() => {
+        setDate(new Date(role.eventDate))
+    }, [role])
+
+    useEffect(() => {
+        dateFormat()
+    }, [date])
+
+    useEffect(() => {
+        console.log(role)
+    }, [])
 
     return (
         <TouchableOpacity className="flex flex-row bg-[#1C1C1C] rounded-2xl" style={{height: height*0.14}} onPress={handleRole}>
             <View className="absolute top-2 left-2 z-10 bg-[#121212]/70 px-2 py-1 rounded-lg">
-                <Text className="text-white text-base">16 DEZ</Text>
+                <Text className="text-white text-base">{dateFormat()}</Text>
             </View>
             <View className="w-1/2">
-                <Image source={{ uri: 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-full h-full object-cover rounded-l-2xl"/>
+                <Image source={{ uri: imageValidation() }} className="w-full h-full object-cover rounded-l-2xl"/>
             </View>
             <View className="w-1/2 p-5">
                 <View className="mb-2">
-                    <Text className="text-xl font-bold text-white">{title}</Text>
+                    <Text 
+                        className="text-xl font-bold text-white"
+                        numberOfLines={1}  // Limita a uma linha
+                        ellipsizeMode="tail"  // Adiciona reticências (...) no final do texto se ultrapassar a linha
+                    >
+                        {role.name}
+                    </Text>
                 </View>
                 <View className="flex flex-row gap-2 mb-4 items-center">
-                    {type === 'Bar' ? (
+                    {role.category === 'Bar' ? (
                         <FontAwesome6 name="beer-mug-empty" size={16} color="white" />
                     )
                     : (
                         <FontAwesome6 name="question" size={16} color="white" />
                     )}
-                    <Text className="text-base text-white">{type}</Text>
+                    <Text className="text-base text-white">{role.category}</Text>
                 </View>
                 <View className="flex flex-row gap-4">
                     <View className="flex flex-row gap-2 items-center">
                         <FontAwesome6 name="star" size={8} color="#BDBDBD" solid/>
-                        <Text className="text-sm text-[#BDBDBD]">{stars}</Text>
+                        <Text className="text-sm text-[#BDBDBD]">4.5</Text>
                     </View>
                     <View className="flex flex-row gap-2 items-center">
                         <FontAwesome6 name="location-dot" size={8} color="#BDBDBD" />
-                        <Text className="text-sm text-[#BDBDBD]">{local}</Text>
+                        <Text className="text-sm text-[#BDBDBD]">{role.districtId}</Text>
                     </View>
                 </View>
             </View>
