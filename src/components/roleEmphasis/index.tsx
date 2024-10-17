05eccbe462d4f19dc54b369af62e380234f5377a
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-10-16 22:29:32 -0300

    Refactor RoleEmphasis component to update date format, add name truncation, and display event details dynamically

--- src/components/roleEmphasis/index.tsx
+++ src/components/roleEmphasis/index.tsx
@@ -1,21 +1,70 @@
 import { FontAwesome6 } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
 import { View, Text, Dimensions, Image, ColorValue } from "react-native";
 
-export default function RoleEmphasis() {
+export default function RoleEmphasis(role: object) {
     const {width, height} = Dimensions.get('window');
+    const [date, setDate] = useState(new Date(role.events[0].date));
+
+    function dateFormat() {
+        const day = date.getDate();
+        const month = date.getMonth();
+        switch (month) {
+            case 0:
+                return `${day} JAN`
+            case 1:
+                return `${day} FEV`
+            case 2:
+                return `${day} MAR`
+            case 3:
+                return `${day} ABR`
+            case 4:
+                return `${day} MAI`
+            case 5:
+                return `${day} JUN`
+            case 6:
+                return `${day} JUL`
+            case 7:
+                return `${day} AGO`
+            case 8:
+                return `${day} SET`
+            case 9:
+                return `${day} OUT`
+            case 10:
+                return `${day} NOV`
+            case 11:
+                return `${day} DEZ`
+        }
+    }
+
+    function nameFormat() {
+        const name = role.events[0].name;
+        if (name.length > 8) {
+            return name.substring(0, 8) + '...'
+        }
+        return name
+    }
+
+    useEffect(() => {
+        console.log(role.events[0]);
+    }, []);
+    
+    useEffect(() => {
+        setDate(new Date(role.events[0].date))
+    }, [role])
 
     return (
         <>
             <View className={`w-full bg-blue-500`} style={{height: height*0.21, borderRadius: 24}} >
                 <View className="absolute top-2 left-2 z-10 bg-[#121212]/70 px-2 py-1 rounded-lg">
-                    <Text className="text-white text-base">16 DEZ</Text>
+                    <Text className="text-white text-base">{dateFormat()}</Text>
                 </View>
-                <Image source={{ uri: 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-full h-full object-cover rounded-3xl"/>
+                <Image source={{ uri: role.events[0].eventPhoto ? role.events[0].eventPhoto : 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-full h-full object-cover rounded-3xl"/>
                 <View className="absolute bottom-2 z-10 w-full">
                     <View className="flex flex-row items-center w-[90%] bg-[#121212]/70 rounded-lg px-2 py-1 mx-auto" style={{height: height*0.05}}>
                         <View className="flex flex-row items-end gap-1 w-[50%] justify-center">
-                            <Text className="text-white text-2xl font-bold">Galleria,</Text>
-                            <Text className="text-[#BEBEBE] text-sm">Pinheiros</Text>
+                            <Text className="text-white text-lg font-bold">{nameFormat()},</Text>
+                            <Text className="text-[#BEBEBE] text-xs">{role.events[0].districtId}</Text>
                         </View>
                         <View className="flex flex-row w-[40%] justify-center items-center">
                             <Image source={{ uri: 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-8 h-8 border-[1px] border-[#1D1D1D99] border-opacity-60 rounded-full"/>
@@ -28,11 +77,11 @@ export default function RoleEmphasis() {
                         <View className="flex flex-col w-[10%] justify-center">
                             <View className="flex flex-row items-center gap-1">
                                 <FontAwesome6 name="star" size={8} color="#BDBDBD" solid/>
-                                <Text className="text-[#BDBDBD] text-sm">4.8</Text>
+                                {role.events[0].rating === null ? <Text className="text-[#BDBDBD] text-sm">0</Text> : <Text className="text-[#BDBDBD] text-sm">{role.events[0].rating}</Text>}
                             </View>
                             <View className="flex flex-row items-center gap-1">
                                 <FontAwesome6 name="user" size={8} color="#BDBDBD" solid/>
-                                <Text className="text-[#BDBDBD] text-sm">195</Text>
+                                <Text className="text-[#BDBDBD] text-sm">{role.events[0].presenceCount}</Text>
                             </View>
                         </View>
                     </View>
