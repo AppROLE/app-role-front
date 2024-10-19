c20888efce96828787ebb64623a98efa9056550a
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-10-19 11:09:49 -0300

    Refactor RoleEmphasis component to update date state and handle undefined events

--- src/components/roleEmphasis/index.tsx
+++ src/components/roleEmphasis/index.tsx
@@ -7,7 +7,7 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function RoleEmphasis(role: object) {
     const {width, height} = Dimensions.get('window');
-    const [date, setDate] = useState(new Date(role.events[0].date));
+    const [date, setDate] = useState(new Date());
 
     function dateFormat() {
         const day = date.getDate();
@@ -58,43 +58,49 @@ export default function RoleEmphasis(role: object) {
     // }, []);
     
     useEffect(() => {
+        if(role.events[0] === undefined) {
+            return
+        }
         setDate(new Date(role.events[0].date))
     }, [role])
 
     return (
         <>
-            <TouchableOpacity className={`w-full bg-blue-500`} style={{height: height*0.21, borderRadius: 24}} onPress={handleRole}>
-                <View className="absolute top-2 left-2 z-10 bg-[#121212]/70 px-2 py-1 rounded-lg">
-                    <Text className="text-white text-base">{dateFormat()}</Text>
-                </View>
-                <Image source={{ uri: role.events[0].eventPhoto ? role.events[0].eventPhoto : 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-full h-full object-cover rounded-3xl"/>
-                <View className="absolute bottom-2 z-10 w-full">
-                    <View className="flex flex-row items-center w-[90%] bg-[#121212]/70 rounded-lg px-2 py-1 mx-auto" style={{height: height*0.05}}>
-                        <View className="flex flex-row items-end gap-1 w-[50%] justify-center">
-                            <Text className="text-white text-lg font-bold">{nameFormat()},</Text>
-                            <Text className="text-[#BEBEBE] text-xs">{role.events[0].districtId}</Text>
-                        </View>
-                        <View className="flex flex-row w-[40%] justify-center items-center">
-                            <Image source={{ uri: 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-8 h-8 border-[1px] border-[#1D1D1D99] border-opacity-60 rounded-full"/>
-                            <Image source={{ uri: 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-8 h-8 border-[1px] border-[#1D1D1D99] border-opacity-60 rounded-full ml-[-8]"/>
-                            <Image source={{ uri: 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-8 h-8 border-[1px] border-[#1D1D1D99] border-opacity-60 rounded-full ml-[-8]"/>
-                            <View className="bg-[#1D1D1D99] w-4 h-4 flex rounded-full ml-[-6]">
-                                <Text className="text-white text-center" style={{lineHeight: 14, fontSize: 12}}>+</Text>
+            {role.events[0] === undefined ? <></>
+                : 
+                <TouchableOpacity className={`w-full bg-blue-500`} style={{height: height*0.21, borderRadius: 24}} onPress={handleRole}>
+                    <View className="absolute top-2 left-2 z-10 bg-[#121212]/70 px-2 py-1 rounded-lg">
+                        <Text className="text-white text-base">{dateFormat()}</Text>
+                    </View>
+                    <Image source={{ uri: role.events[0].eventPhoto ? role.events[0].eventPhoto : 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-full h-full object-cover rounded-3xl"/>
+                    <View className="absolute bottom-2 z-10 w-full">
+                        <View className="flex flex-row items-center w-[90%] bg-[#121212]/70 rounded-lg px-2 py-1 mx-auto" style={{height: height*0.05}}>
+                            <View className="flex flex-row items-end gap-1 w-[50%] justify-center">
+                                <Text className="text-white text-lg font-bold">{nameFormat()},</Text>
+                                <Text className="text-[#BEBEBE] text-xs">{role.events[0].districtId}</Text>
                             </View>
-                        </View>
-                        <View className="flex flex-col w-[10%] justify-center">
-                            <View className="flex flex-row items-center gap-1">
-                                <FontAwesome6 name="star" size={8} color="#BDBDBD" solid/>
-                                {role.events[0].rating === null ? <Text className="text-[#BDBDBD] text-sm">0</Text> : <Text className="text-[#BDBDBD] text-sm">{role.events[0].rating}</Text>}
+                            <View className="flex flex-row w-[40%] justify-center items-center">
+                                <Image source={{ uri: 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-8 h-8 border-[1px] border-[#1D1D1D99] border-opacity-60 rounded-full"/>
+                                <Image source={{ uri: 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-8 h-8 border-[1px] border-[#1D1D1D99] border-opacity-60 rounded-full ml-[-8]"/>
+                                <Image source={{ uri: 'https://d2sw4frthbnrzj.cloudfront.net/teste/role_bombando_teste.png' }} className="w-8 h-8 border-[1px] border-[#1D1D1D99] border-opacity-60 rounded-full ml-[-8]"/>
+                                <View className="bg-[#1D1D1D99] w-4 h-4 flex rounded-full ml-[-6]">
+                                    <Text className="text-white text-center" style={{lineHeight: 14, fontSize: 12}}>+</Text>
+                                </View>
                             </View>
-                            <View className="flex flex-row items-center gap-1">
-                                <FontAwesome6 name="user" size={8} color="#BDBDBD" solid/>
-                                <Text className="text-[#BDBDBD] text-sm">{role.events[0].presenceCount}</Text>
+                            <View className="flex flex-col w-[10%] justify-center">
+                                <View className="flex flex-row items-center gap-1">
+                                    <FontAwesome6 name="star" size={8} color="#BDBDBD" solid/>
+                                    {role.events[0].rating === null ? <Text className="text-[#BDBDBD] text-sm">0</Text> : <Text className="text-[#BDBDBD] text-sm">{role.events[0].rating}</Text>}
+                                </View>
+                                <View className="flex flex-row items-center gap-1">
+                                    <FontAwesome6 name="user" size={8} color="#BDBDBD" solid/>
+                                    <Text className="text-[#BDBDBD] text-sm">{role.events[0].presenceCount}</Text>
+                                </View>
                             </View>
                         </View>
                     </View>
-                </View>
-            </TouchableOpacity>
+                </TouchableOpacity>
+            }
         </>
     )
 }
