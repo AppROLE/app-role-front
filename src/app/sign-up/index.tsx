47b95ae7ae9d6522fe4f401de32082c96d4ea93f
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-10-19 21:07:28 -0300

    Credentials verification and Toast fixed

--- src/app/sign-up/index.tsx
+++ src/app/sign-up/index.tsx
@@ -1,16 +1,16 @@
-import { signUpRequestDTO } from '@/api/types/auth_dto'
+// import { signUpRequestDTO } from '@/api/types/auth_dto'
 import { AuthContext } from '@/context/auth_context'
 import TermsAndConditionsModal from '@/src/components/TermsAndConditionsModal'
 import Background from '@/src/components/background'
 import RoleInput from '@/src/components/input'
 import RoleMainButton from '@/src/components/roleMainButton'
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6'
-import Ionicons from '@expo/vector-icons/Ionicons'
 import { Link, router } from 'expo-router'
 import React from 'react'
-import { View, Pressable, Text } from 'react-native'
+import {View, Pressable, Text, ActivityIndicator} from 'react-native'
 import Toast, { ErrorToast } from 'react-native-toast-message'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import Svg from '@/src/components/svg'
 
 export default function SignUp() {
   const [isVisible, setIsVisible] = React.useState(false)
@@ -35,7 +35,9 @@ export default function SignUp() {
     const numberRegex = /[0-9]/
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if(user.length == 8){
+    setUser(user.trim())
+
+    if(user.trim().length == 0){
       setUserError("Nome inválido")
       isValid = false
     }
@@ -78,7 +80,6 @@ export default function SignUp() {
   }
 
   async function createAccount() {
-    if(buttonDebounce) return
     setButtonDebounce(true)
     
     if (!verifyCredentials()){
@@ -117,10 +118,10 @@ export default function SignUp() {
         visibilityTime: 3000,
         type: 'error',
         props: {
-          style: { backgroundColor: 'black' }, 
-          text1Style: { color: 'white', fontSize: 18, fontFamily: "nunito" }, 
+          style: { backgroundColor: 'black' },
+          text1Style: { color: 'white', fontSize: 18, fontFamily: "nunito" },
           text2Style: { color: 'white', fontFamily: "nunito" }, 
-          contentContainerStyle: { backgroundColor: 'black' } 
+          contentContainerStyle: { backgroundColor: 'white' }
         },
       });
     }
@@ -156,24 +157,6 @@ export default function SignUp() {
   return (
     <Background>
       <View className="flex h-[89%] w-full flex-col items-center gap-10 rounded-t-[54px] bg-background">
-        <Toast config={{
-          error: (props) => (
-            <ErrorToast
-              {...props}
-              style={{ backgroundColor: 'purple'}}
-              text1Style={{
-                fontSize: 17,
-                color: 'white',
-                fontFamily: 'nunito', 
-              }}
-              text2Style={{
-                fontSize: 15,
-                color: 'white',
-                fontFamily: 'nunito',
-              }}
-            />
-          ),
-        }}/>
         <View className="flex items-center">
           <View className="flex flex-row gap-[5px]">
             <Text className="text-2xl font-nunitoBold text-white max-[320px]:text-xl">
@@ -225,9 +208,19 @@ export default function SignUp() {
             <View className="mt-5 w-2/3 flex flex-row items-center justify-center gap-3">
               <Pressable onPress={() => {setIsChecked(!isChecked); setCheckedError(false)}}>
                 {isChecked ? (
-                  <Ionicons name="checkbox-outline" size={24} color="white" />
+                  <Svg
+                      uri={process.env.EXPO_PUBLIC_URL_S3 + "/checkbox_check.svg"}
+                      color={'#BDBDBD'}
+                      width={24}
+                      height={20}
+                  />
                 ) : (
-                  <Ionicons name="square-outline" size={24} color="white" />
+                  <Svg
+                      uri={process.env.EXPO_PUBLIC_URL_S3 + "/checkbox_empty.svg"}
+                      color={'#BDBDBD'}
+                      width={24}
+                      height={20}
+                  />
                 )}
               </Pressable>
               <Text className="flex items-center font-nunito justify-center text-xs text-white">
@@ -247,8 +240,13 @@ export default function SignUp() {
           <RoleMainButton
             type="gradient"
             buttonFunction={() => createAccount()}
+            disabled={buttonDebounce}
           >
-            <Text className="text-white font-nunito">Cadastrar</Text>
+            {buttonDebounce ?
+                <ActivityIndicator color={'white'}/>
+                :
+                <Text className="text-white font-nunito">Cadastrar</Text>
+            }
           </RoleMainButton>
           <RoleMainButton type="simple">
             <FontAwesome6 name="google" size={24} color="white" />
@@ -262,6 +260,26 @@ export default function SignUp() {
           </Link>
         </View>
       </View>
+      <Toast config={{
+        error: (props) => (
+            <ErrorToast
+                {...props}
+                style={{
+                  backgroundColor: '#240046',
+                }}
+                text1Style={{
+                  fontSize: 17,
+                  color: 'white',
+                  fontFamily: 'NunitoBold',
+                }}
+                text2Style={{
+                  fontSize: 15,
+                  color: 'white',
+                  fontFamily: 'Nunito',
+                }}
+            />
+        ),
+      }}/>
       <TermsAndConditionsModal isVisible={isVisible} />
     </Background>
   )
